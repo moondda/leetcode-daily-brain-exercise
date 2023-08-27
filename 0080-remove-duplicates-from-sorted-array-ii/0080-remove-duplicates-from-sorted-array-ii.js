8b3cbd7dac2d9930ea571692c076a9923d3d8230
moondda <93575538+moondda@users.noreply.github.com>
2023-08-27 18:21:57 +0900

    Time: 70 ms (36.40%), Space: 43.7 MB (80.51%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    
+    let k = 2;
+    
+    //[0,1,2] pass 
+    //[0,1,1] pass
+    //[0,0,1] pass
+    //[0,0,0] *
+
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] != nums[k - 2]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
