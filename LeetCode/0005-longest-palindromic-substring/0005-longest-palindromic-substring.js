765ca8c089a28f0dda3fc506bcff23f2ecddfbde
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-05-16 01:29:24 +0900

    Time: 37 ms (36.97%), Space: 62.5 MB (19.31%) - LeetHub

--- LeetCode/0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ LeetCode/0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -3,16 +3,16 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
+    if(s.length === 1) return s[0]; 
     let valid = s[0];
     //중간이 있을때 aba
     for(let i=1;i<s.length-1;i++){
         let left = i-1;
         let mid = i;
         let right = i+1;
-        while(s[left] === s[right]) {
+        while(left >= 0 && right <= s.length -1 && s[left] === s[right]) {
             let target = s.slice(left,right+1);
             if(target.length > valid.length) valid = target;
-            if(left-1 < 0 || right+1 > s.length-1) break;
             left = left-1;
             right = right+1;
         }
@@ -22,10 +22,9 @@ var longestPalindrome = function(s) {
     for(let i=0;i<s.length-1;i++){
         let left = i;
         let right = i+1;
-        while(s[left] === s[right]) {
+        while(left >= 0 && right <= s.length -1 && s[left] === s[right]) {
             let target = s.slice(left,right+1);
             if(target.length > valid.length) valid = target;
-            if(left-1 < 0 || right+1 > s.length-1) break;
             left = left-1;
             right = right+1;
         }
